141fd41fc8ed997b36e9eac2a46ab412b8dc9ec6
128bit <128bit@eclipso.eu>
2023-11-22 15:38:34 +0300

    Add TodoObject type, TodoItem and Project classes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-console.log("Hello World!");
+interface TodoObject {
+  title: string;
+  description: string;
+  dueDate: Date | null;
+  dateStarted: Date;
+  priority: "low" | "normal" | "high";
+  notes: string[];
+  checklist: string[];
+}
+
+class TodoItem implements TodoObject {
+  title: string;
+  description: string;
+  dueDate: Date | null;
+  dateStarted: Date;
+  priority: "low" | "normal" | "high";
+  notes: string[];
+  checklist: string[];
+  constructor({
+    title,
+    description,
+    dueDate,
+    dateStarted,
+    priority,
+    notes = [],
+    checklist = [],
+  }: TodoObject) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.dateStarted = dateStarted;
+    this.priority = priority;
+    this.notes = notes;
+    this.checklist = checklist;
+  }
+}
+
+class Project {
+  items: TodoItem[];
+  constructor() {
+    this.items = [];
+  }
+}
