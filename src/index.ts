8f92efa8302779e75d1621cd426add345d259ca2
128bit <128bit@eclipso.eu>
2023-11-23 19:18:49 +0300

    Add prototype code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-import { TodoItem, Project } from './classes'
+import { TodoItem, Project, ProjectList } from './classes'
+// import { type priority, type TodoObject, type checklistItem } from './interfaces'
+import createTodoObject from './createTodoObject'
+import 'normalize.css'
+
+const form = document.querySelector('form') as HTMLFormElement
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const formData = new FormData(e.target as HTMLFormElement)
+  const initParams = createTodoObject(formData)
+  console.log(new TodoItem(initParams))
+})
