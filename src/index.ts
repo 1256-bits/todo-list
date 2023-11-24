9da91261d3cccfbf0a5f4f6642568cacbb6e072a
128bit <128bit@eclipso.eu>
2023-11-24 11:33:43 +0300

    Update mockup code

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,31 @@ import { TodoItem, Project, ProjectList } from './classes'
 import createTodoObject from './createTodoObject'
 import 'normalize.css'
 
-const form = document.querySelector('form') as HTMLFormElement
-form.addEventListener('submit', e => {
+const projectList = new ProjectList()
+const project = new Project('default', 0)
+projectList.addProject(project)
+
+const form = document.querySelector('#new-todo')
+form?.addEventListener('submit', e => {
   e.preventDefault()
   const formData = new FormData(e.target as HTMLFormElement)
   const initParams = createTodoObject(formData)
-  console.log(new TodoItem(initParams))
+  project.addItem(new TodoItem(initParams))
+})
+
+const newProject = document.querySelector('#new-project')
+newProject?.addEventListener('submit', form => {
+  form.preventDefault()
+  const formData = new FormData(form.target as HTMLFormElement)
+  const sanitize = (data: FormDataEntryValue | null): string => (data !== "" && data) ? String(data) : 'New project'
+  const project = new Project(sanitize(formData.get('title')), Date.now())
+  projectList.addProject(project)
+})
+
+const listProjects = document.querySelector('#list-projects')
+listProjects?.addEventListener('click', () => {
+  const json = JSON.stringify(projectList)
+  const newPL = new ProjectList()
+  newPL.fromJSON(json)
+  console.log(newPL)
 })
