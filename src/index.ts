5dc40ede9a4252d9f4eb1ea1d73edf1a245c97b4
128bit <128bit@eclipso.eu>
2023-11-24 13:40:35 +0300

    Import styles and DOM

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { TodoItem, Project, ProjectList } from './classes'
 // import { type priority, type TodoObject, type checklistItem } from './interfaces'
+import * as DOM from './dom'
 import createTodoObject from './createTodoObject'
 import 'normalize.css'
+import 'styles.scss'
 
 const projectList = new ProjectList()
 const project = new Project('default', 0)
@@ -19,15 +21,10 @@ const newProject = document.querySelector('#new-project')
 newProject?.addEventListener('submit', form => {
   form.preventDefault()
   const formData = new FormData(form.target as HTMLFormElement)
-  const sanitize = (data: FormDataEntryValue | null): string => (data !== "" && data) ? String(data) : 'New project'
+  const sanitize = (data: FormDataEntryValue | null): string => (data !== '' && typeof data === 'string') ? data : 'New project'
   const project = new Project(sanitize(formData.get('title')), Date.now())
   projectList.addProject(project)
 })
 
 const listProjects = document.querySelector('#list-projects')
-listProjects?.addEventListener('click', () => {
-  const json = JSON.stringify(projectList)
-  const newPL = new ProjectList()
-  newPL.fromJSON(json)
-  console.log(newPL)
-})
+listProjects?.addEventListener('click', DOM.listProjects)
