8248f66b4da13a127da1d03769cd7735d7a7f7c8
128bit <128bit@eclipso.eu>
2023-12-23 15:34:45 +0300

    Add createEditButton, createDeleteButton, add title and id to nameBtn

--- src/project-item.ts
+++ src/project-item.ts
@@ -1,17 +1,38 @@
 import { Project } from './classes'
+import createIcon from './create-icon'
+
 export default function createProjectItem (project: Project): HTMLLIElement {
   const li = document.createElement('li')
   li.classList.add('project')
 
   const nameBtn = document.createElement('button')
   nameBtn.classList.add('project-name', 'italic')
+  nameBtn.innerText = project.title
+  nameBtn.setAttribute('data-id', String(project.id))
 
+  const editBtn = createEditButton()  
+  const delBtn = createDeleteButton()
 
-  const renameBtn = document.createElement('button')
-  renameBtn.classList.add('rename-button')
-
-  const deleteBtn = document.createElement('button')
-  deleteBtn.classList.add('delete-button')
-
+  li.append(editBtn, delBtn)
   return li
+}
+
+function createEditButton (): HTMLButtonElement {
+  const button = document.createElement('button')
+  button.classList.add('rename-button')
+
+  const svg = createIcon('edit')
+  button.appendChild(svg)
+  
+  return button
+}
+
+function createDeleteButton (): HTMLButtonElement {
+  const button = document.createElement('button')
+  button.classList.add('delete-button')
+  
+  const svg = createIcon('delete')
+  button.appendChild(svg)
+
+  return button
 }
