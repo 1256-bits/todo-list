0d9f802148793d0291b0b9794967a2a07b39e975
128bit <128bit@eclipso.eu>
2023-12-23 17:38:54 +0300

    Create XML elements instead of HTML elements

--- src/create-icon.ts
+++ src/create-icon.ts
@@ -1,11 +1,11 @@
-export default function createIcon (type: 'add' | 'edit' | 'delete'): HTMLElement {
+export default function createIcon (type: 'add' | 'edit' | 'delete'): SVGSVGElement {
   const href = { add: './svg/add.svg#add', edit: './svg/edit-pencil.svg#edit', delete: './svg/delete.svg#delete' }
-  const svg = document.createElement('svg')
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
   svg.setAttribute('viewBox', '0 0 20 20')
 
-  const use = document.createElement('use')
-  use.setAttribute('height', '20')
+  const use = document.createElementNS('http://www.w3.org/2000/svg', 'use')
   use.setAttribute('width', '20')
+  use.setAttribute('height', '20')
   use.setAttribute('href', href[type])
 
   svg.appendChild(use)
