09e47566cea85be3bc4a1cb761916a951a561c16
128bit <128bit@eclipso.eu>
2023-12-23 22:21:28 +0300

    Add dummy todo item with a checklist

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,6 @@ import 'normalize.css'
 import './styles.scss'
 
 const projectList = new ProjectList()
-if (projectList.items.length === 0) {
-  const project = new Project('default', 0)
-  projectList.addProject(project)
-}
 
 /*
 const form = document.querySelector('#new-todo')
@@ -20,7 +16,6 @@ form?.addEventListener('submit', e => {
   const initParams = createTodoObject(formData)
   project.addItem(new TodoItem(initParams))
 })
-*/
 
 const newProject = document.querySelector('#new-project')
 newProject?.addEventListener('submit', form => {
@@ -30,6 +25,7 @@ newProject?.addEventListener('submit', form => {
   const project = new Project(sanitize(formData.get('title')), Date.now())
   projectList.addProject(project)
 })
+*/
 
 const listProjects = document.querySelector('#list-projects')
 // listProjects?.addEventListener('click', DOM.listProjects)
@@ -38,5 +34,17 @@ projectList.addProject(new Project('Project 1', Date.now()))
 projectList.addProject(new Project('Project 2', Date.now()))
 projectList.addProject(new Project('Project 3', Date.now()))
 
+const item = new TodoItem(createTodoObject(new FormData))
+item.adddChecklistItem('Item 1')
+item.adddChecklistItem('Item 2')
+projectList.items[0].addItem(item)
+
 const projectUL = document.querySelector('nav > ul')
 projectList.items.forEach(item => projectUL?.appendChild(createProjectItem(item)))
+//const projectNames = document.querySelectorAll('project-name')
+//projectNames.forEach(name => name.addEventListener('click', name => )
+
+if (projectList.items.length === 0) {
+  const project = new Project('default', 0)
+  projectList.addProject(project)
+}
