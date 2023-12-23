371ae2abdb5ef0421825e76d1ff3e573e20b5708
128bit <128bit@eclipso.eu>
2023-12-23 15:23:00 +0300

    Add svg icon module

---
+++ src/edit-icon.ts
@@ -0,0 +1,13 @@
+export default function createIcon (type: 'add' | 'edit' | 'delete'): HTMLElement {
+  const href = { add: './svg/add.svg#add', edit: "./svg/edit-pencil.svg#edit", delete: "./svg/delete.svg#delete" }
+  const svg = document.createElement('svg')
+  svg.setAttribute('viewBox', '0 0 20 20')
+
+  const use = document.createElement('use')
+  use.setAttribute('height', '20')
+  use.setAttribute('width', '20')
+  use.setAttribute('href', href[type])
+
+  svg.appendChild(use)
+  return svg
+}
