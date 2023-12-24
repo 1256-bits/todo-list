b1f066661ff2deaac027282cef363d8c52b4b0c0
128bit <128bit@eclipso.eu>
2023-12-24 13:36:14 +0300

    Add main todo display functions

--- src/listTodos.ts
+++ src/listTodos.ts
@@ -1,25 +1,57 @@
-import { type ProjectList, type TodoItem } from './classes'
-// import createIcon from './create-icon'
-declare const projectList: ProjectList
+import { type TodoItem } from './classes'
+import { projectList } from './index'
+import createButtonElement from './createButtonElement'
 
 export default function listTodos (e: Event): void {
   if (!(e.target instanceof HTMLElement) || e.target.dataset.id == null) {
     console.error('listTodos: not an HTML element or missing ID')
     return
   }
+  const todoArea = document.querySelector('main > ul')
   const id = parseInt(e.target.dataset.id)
   const project = projectList.items.filter(item => item.id === id)[0]
   project.items.forEach(item => {
-    displayTodo(item)
+    todoArea?.appendChild(createTodo(item))
   })
 }
 
-function displayTodo (item: TodoItem): void {
-  /*
-  const todoArea = document.querySelector('main > ul')
+function createTodo (item: TodoItem): HTMLLIElement {
   const todoItem = document.createElement('li')
+  todoItem.classList.add('todo-item')
+
+  const done = createCheckbox()
+  const doneLabelHidden = createCheckboxLabel()
+  const name = createNameField(item.title)
+  const addBtn = createButtonElement('add')
+  const renameBtn = createButtonElement('rename')
+  const deleteBtn = createButtonElement('delete')
+  
+  todoItem.append(done, doneLabelHidden, name, addBtn, renameBtn, deleteBtn)
+  return todoItem
+}
+
+function createCheckbox (): HTMLInputElement {
   const done = document.createElement('input')
-  const doneLabelHidden = document.createElement('label')
-  */
-  // const addButton = createAddButton()
+  done.setAttribute('type', 'checkbox')
+  done.setAttribute('name', 'done')
+
+  return done
+}
+
+function createCheckboxLabel (): HTMLLabelElement {
+  const label = document.createElement('label')
+  label.setAttribute('for', 'done')
+  label.classList.add('hidden')
+  label.innerText = 'Done'
+
+  return label
+}
+
+function createNameField (title: string): HTMLInputElement {
+  const name = document.createElement('input')
+  name.classList.add('todo-name')
+  name.readOnly = true
+  name.value = title
+
+  return name
 }
