00c3a6f3652449ae2a7bf46d98fd11766537d414
128bit <128bit@eclipso.eu>
2024-03-20 14:00:23 +0300

    Move default export to the top

--- src/listProjects.ts
+++ src/listProjects.ts
@@ -2,6 +2,20 @@ import { type Project } from './classes'
 import createButtonElement from './createButtonElement'
 import { projectList } from './index'
 
+export default function listProjects (): void {
+  const projectArea = document.querySelector('.projects-subarea')
+
+  if (projectArea == null) {
+    console.error('projectArea does not exist')
+    return
+  }
+
+  projectArea.innerHTML = ''
+  projectList.items.forEach(item => {
+    projectArea.appendChild(createProjectItem(item))
+  })
+}
+
 function createProjectItem (project: Project): HTMLLIElement {
   const li = document.createElement('li')
   li.classList.add('project')
@@ -16,18 +30,4 @@ function createProjectItem (project: Project): HTMLLIElement {
 
   li.append(nameBtn, renameBtn, delBtn)
   return li
-}
-
-export default function listProjects (): void {
-  const projectArea = document.querySelector('.projects-subarea')
-
-  if (projectArea == null) {
-    console.error('projectArea does not exist')
-    return
-  }
-  
-  projectArea.innerHTML = ''
-  projectList.items.forEach(item => {
-    projectArea.appendChild(createProjectItem(item))
-  })
 }
