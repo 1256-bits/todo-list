593600b14d15a6717e61e074d23ba7c8d227067c
128bit <128bit@eclipso.eu>
2024-03-23 16:47:11 +0300

    Separate checklist-related functions

---
+++ src/checklistHandlers.ts
@@ -0,0 +1,69 @@
+import { projectList } from '.'
+import { listTodos } from './dom'
+
+export function addChecklistItem (e: Event): void {
+  const newChecklistDialog = document.querySelector('#new-checklist') as HTMLDialogElement
+  const target = e.currentTarget as HTMLElement
+  const id = target.parentElement?.dataset.id
+  newChecklistDialog.showModal()
+  newChecklistDialog.addEventListener('submit', e => {
+    if (id == null) {
+      return
+    }
+    addChecklistHandler(e, id)
+  }, { once: true })
+}
+
+export function renameChecklistItem (e: Event): void {
+  const editChecklistDialog = document.querySelector('#edit-checklist') as HTMLDialogElement
+  const target = e.currentTarget as HTMLElement
+  const id = target.parentElement?.dataset.id
+  const index = target.parentElement?.dataset.index
+  editChecklistDialog.showModal()
+  editChecklistDialog.addEventListener('submit', e => {
+    if (id == null || index == null) {
+      return
+    }
+    editChecklistHandler(e, id, parseInt(index))
+  }, { once: true })
+}
+
+export function deleteChecklistItem (): void {
+}
+
+function addChecklistHandler (e: Event, id: string): void {
+  const target = e.target as HTMLFormElement
+  const formData = new FormData(target)
+  const content = formData.get('content') as string
+  if (content == null) {
+    target.reset()
+    return
+  }
+  const projectId = localStorage.getItem('currentProjectId')
+  if (projectId == null) {
+    console.error('Current project ID not found')
+    return
+  }
+  const todo = projectList.getProject(projectId).getItem(id)
+  todo.addChecklistItem(content)
+  listTodos(projectId)
+}
+
+function editChecklistHandler (e: Event, id: string, index: number): void {
+  const target = e.target as HTMLFormElement
+  const formData = new FormData(target)
+  const content = formData.get('content') as string
+  if (content == null) {
+    target.reset()
+    return
+  }
+  const projectId = localStorage.getItem('currentProjectId')
+  if (projectId == null) {
+    console.error('Current project ID not found')
+    return
+  }
+  const todo = projectList.getProject(projectId).getItem(id)
+  const item = todo.findChecklistItem(index)
+  item.text = content
+  listTodos(projectId)
+}
--- src/dom.ts
+++ src/dom.ts
@@ -1,67 +1,12 @@
 import listProjects, { deleteProject, renameHandler } from './listProjects'
 import listTodos from './listTodos'
 import getIdFromEvent from './getIdFromEvent'
-import { projectList } from '.'
+import { addChecklistItem, deleteChecklistItem, renameChecklistItem } from './checklistHandlers'
 
-export { listTodos, getIdFromEvent, listProjects }
+export { listTodos, getIdFromEvent, listProjects, addChecklistItem, renameChecklistItem, deleteChecklistItem }
 
 const headerRenameBtn = document.querySelector('.project-header > .rename-button')
 const headerDeleteBtn = document.querySelector('.project-header > .delete-button')
 
 headerRenameBtn?.addEventListener('click', renameHandler)
 headerDeleteBtn?.addEventListener('click', deleteProject)
-
-export function addChecklistItem (e: Event): void {
-  const newChecklistDialog = document.querySelector('#new-checklist') as HTMLDialogElement
-  const target = e.currentTarget as HTMLElement
-  const id = target.parentElement?.dataset.id
-  newChecklistDialog.showModal()
-  newChecklistDialog.addEventListener('submit', e => {
-    if (id == null) {
-      return
-    }
-    checklistHandler(e, id, 'add')
-  }, { once: true })
-}
-
-export function renameChecklistItem (e: Event): void {
-  const editChecklistDialog = document.querySelector('#edit-checklist') as HTMLDialogElement
-  const target = e.currentTarget as HTMLElement
-  const id = target.parentElement?.dataset.id
-  const index = target.parentElement?.dataset.index as string
-  editChecklistDialog.showModal()
-  editChecklistDialog.addEventListener('submit', e => {
-    if (id == null) {
-      return
-    }
-    checklistHandler(e, id, 'edit', parseInt(index))
-  }, { once: true })
-}
-
-export function deleteChecklistItem (): void {
-}
-
-function checklistHandler (e: Event, id: string, type: 'add'): void
-function checklistHandler (e: Event, id: string, type: 'edit', index: number): void
-function checklistHandler (e: Event, id: string, type: 'add' | 'edit', index?: number): void {
-  const target = e.target as HTMLFormElement
-  const formData = new FormData(target)
-  const content = formData.get('content') as string
-  if (content == null) {
-    target.reset()
-    return
-  }
-  const projectId = localStorage.getItem('currentProjectId')
-  if (projectId == null) {
-    console.error('Current project ID not found')
-    return
-  }
-  const todo = projectList.getProject(projectId).getItem(id)
-  if (type === 'add') {
-    todo.addChecklistItem(content)
-  } else if (index != null) {
-    const item = todo.findChecklistItem(index)
-    item.text = content
-  }
-  listTodos(projectId)
-}
