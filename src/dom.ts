3895d17d50c4bab3672fdc5d135442bada452cae
128bit <128bit@eclipso.eu>
2024-03-23 13:53:05 +0300

    Attach listeners to project header buttons

--- src/dom.ts
+++ src/dom.ts
@@ -1,5 +1,11 @@
-import listProjects from './listProjects'
+import listProjects, { deleteProject, renameHandler } from './listProjects'
 import listTodos from './listTodos'
 import getIdFromEvent from './getIdFromEvent'
 
 export { listTodos, getIdFromEvent, listProjects }
+
+const headerRenameBtn = document.querySelector('.project-header > .rename-button')
+const headerDeleteBtn = document.querySelector('.project-header > .delete-button')
+
+headerRenameBtn?.addEventListener('click', renameHandler)
+headerDeleteBtn?.addEventListener('click', deleteProject)
