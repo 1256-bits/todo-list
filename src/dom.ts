3dab9af9dc7a70f7ec7b9369548f85ab26e408a5
128bit <128bit@eclipso.eu>
2024-03-25 17:27:34 +0300

    Use re-export syntax

--- src/dom.ts
+++ src/dom.ts
@@ -1,13 +1,5 @@
-import listProjects from './listProjects'
-import { deleteProject, renameHandler } from './projectHandlers'
-import listTodos from './listTodos'
-import { addChecklistItem, deleteChecklistItem, renameChecklistItem } from './checklistHandlers'
-import { createTodoHandler, newTodoBtnClickHandler } from './todoHandlers'
-
-export { listTodos, listProjects, addChecklistItem, renameChecklistItem, deleteChecklistItem, createTodoHandler, newTodoBtnClickHandler, renameHandler, deleteProject }
-
-const headerRenameBtn = document.querySelector('.project-header > .rename-button')
-const headerDeleteBtn = document.querySelector('.project-header > .delete-button')
-
-headerRenameBtn?.addEventListener('click', renameHandler)
-headerDeleteBtn?.addEventListener('click', deleteProject)
+export { default as listProjects } from './listProjects'
+export { default as listTodos } from './listTodos'
+export * from './projectHandlers'
+export * from './checklistHandlers'
+export * from './todoHandlers'
