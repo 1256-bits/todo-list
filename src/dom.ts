1e9a02788235913545d4cc4fe37be9a40e8a6d92
128bit <128bit@eclipso.eu>
2024-03-25 16:45:57 +0300

    Add todoHandlers

--- src/dom.ts
+++ src/dom.ts
@@ -2,8 +2,9 @@ import listProjects, { deleteProject, renameHandler } from './listProjects'
 import listTodos from './listTodos'
 import getIdFromEvent from './getIdFromEvent'
 import { addChecklistItem, deleteChecklistItem, renameChecklistItem } from './checklistHandlers'
+import { createTodoHandler, newTodoBtnClickHandler } from './todoHandlers'
 
-export { listTodos, getIdFromEvent, listProjects, addChecklistItem, renameChecklistItem, deleteChecklistItem }
+export { listTodos, getIdFromEvent, listProjects, addChecklistItem, renameChecklistItem, deleteChecklistItem, createTodoHandler, newTodoBtnClickHandler }
 
 const headerRenameBtn = document.querySelector('.project-header > .rename-button')
 const headerDeleteBtn = document.querySelector('.project-header > .delete-button')
