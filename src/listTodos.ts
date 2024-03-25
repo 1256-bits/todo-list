92276a63678a877170b6544561bb14cb082851a8
128bit <128bit@eclipso.eu>
2024-03-25 16:14:48 +0300

    Add a function to paste data into editing form

--- src/listTodos.ts
+++ src/listTodos.ts
@@ -1,8 +1,8 @@
 import { type TodoItem } from './classes'
-import { getCurrentProjectId, projectList } from './index'
+import { getCurrentProjectId, parseDate, projectList } from './index'
 import createButtonElement from './createButtonElement'
 import { type priority, type checklistItem } from './interfaces'
-import { addChecklistItem, renameChecklistItem, deleteChecklistItem } from './dom'
+import { addChecklistItem, renameChecklistItem, deleteChecklistItem, getIdFromEvent } from './dom'
 
 export default function listTodos (id: string): void {
   const todoArea = document.querySelector('main > ul')
@@ -139,7 +139,37 @@ function createNameField (title: string): HTMLInputElement {
   return name
 }
 
-function editTodoItem (): void {
+function editTodoItem (e: Event): void {
+  const dialog = document.querySelector('#new-todo-dialog')
+  const saveBtn = dialog?.querySelector('#create-todo')
+  const id = getIdFromEvent(e)
+  if (!(dialog instanceof HTMLDialogElement) || !(saveBtn instanceof HTMLButtonElement)) {
+    throw new Error('Dialog or button not found')
+  }
+  assignValues(id, dialog)
+  saveBtn.textContent = 'Save'
+  dialog.showModal()
+}
+
+function assignValues (id: string, dialog: HTMLDialogElement): void {
+  const projectId = getCurrentProjectId()
+  const todo = projectList.getProject(projectId).getItem(id)
+
+  const title = dialog.querySelector('input[name="title"]') as HTMLInputElement
+  const description = dialog.querySelector('input[name="description"]') as HTMLInputElement
+  const dueDate = dialog.querySelector('input[name="dueDate"]') as HTMLInputElement
+  const dateStarted = dialog.querySelector('input[name="dateStarted"]') as HTMLInputElement
+  const priority = dialog.querySelector(`fieldset input[value="${todo.priority}"]`) as HTMLInputElement
+  const notes = dialog.querySelector('textarea') as HTMLTextAreaElement
+
+  title.value = todo.title
+  description.value = todo.description
+  if (todo.dueDate != null) {
+    dueDate.value = parseDate(todo.dueDate)
+  }
+  dateStarted.value = parseDate(todo.dateStarted)
+  priority.checked = true
+  notes.value = todo.notes
 }
 
 function deleteTodoItem (e: Event): void {
